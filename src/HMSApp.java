ff6d8a6e1935211ffc00c61c0df0f5bbb4667331
Bryan Soong <whiterevolver35@gmail.com>
2024-10-30 01:34:51 +0800

    readd the patient entry

--- src/HMSApp.java
+++ src/HMSApp.java
@@ -1,16 +1,22 @@
+import services.Navigator;
+import view.LoginView;
+
 /**
  * The starting point of the application.
  * @author
  * @version 1.0
  * @since 2024-10-18
  */
-
-import services.Navigator;
-import view.LoginView;
-
-
 public class HMSApp {
     public static void main(String[] args) throws Exception {
         Navigator.navigateTo(new LoginView());
+        
+        /*
+        UserRepository rp = new UserRepository();  
+        rp.save(new Patient("P1001", "Alice Brown", "123", 
+                            Gender.FEMALE, LocalDate.of(1980, 5, 14), 
+                            "81888888", "alice.brown@example.com", BloodType.A_POSITIVE));
+        */
+        
     }
 }
