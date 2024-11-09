6dc4455985cad068c351ded9dc1272cfa416a6e2
Bryan Soong <whiterevolver35@gmail.com>
2024-11-09 10:57:24 +0800

    :hammer: refactor

--- src/view/Admin/AdminView.java
+++ src/view/Admin/AdminView.java
@@ -3,15 +3,9 @@ package view.Admin;
 import controller.UserManager;
 import lib.uilib.framework.BuildContext;
 import lib.uilib.framework.MenuOption;
-import lib.uilib.framework.TableRow;
-import lib.uilib.framework.enums.Alignment;
 import lib.uilib.framework.enums.TextStyle;
 import lib.uilib.widgets.base.Menu;
-import lib.uilib.widgets.base.Table;
 import lib.uilib.widgets.base.Text;
-import lib.uilib.widgets.base.VSpacer;
-import lib.uilib.widgets.layout.Align;
-import model.Admin;
 import services.Navigator;
 import view.LoginView;
 import view.View;
@@ -43,7 +37,7 @@ public class AdminView extends View {
 
     @Override
     public String getViewName() {
-        return("AdminView");
+        return("Admin");
     }
 
     @Override
@@ -51,33 +45,23 @@ public class AdminView extends View {
 
         BuildContext context = new BuildContext(100, 10);
 
-        final int[] validChoice = {-1};
-        
-        while(validChoice[0] != 1){
-            new Menu(
-                new MenuOption("View and Manage Hospital Staff", () -> {
-                    this.viewAndManageStaff();; validChoice[0] = 1;}),
+        new Menu(
+            new MenuOption("View and Manage Hospital Staff", () -> 
+                this.viewAndManageStaff()),
 
-                new MenuOption("View Appoinments Details", () -> {
-                    this.viewAppointmentsDetails();; validChoice[0] = 1;}),
+            new MenuOption("View Appoinments Details", () -> 
+                this.viewAppointmentsDetails()),
 
-                new MenuOption("View and Manage Medication Inventory", () -> {
-                    this.viewandManageInventory();; validChoice[0] = 1;}),
+            new MenuOption("View and Manage Medication Inventory", () ->
+                this.viewandManageInventory()),
 
-                new MenuOption("Approve Replenishment Request", () -> {
-                    this.approveReplenishRequest();; validChoice[0] = 1;}),
+            new MenuOption("Approve Replenishment Request", () -> 
+                this.approveReplenishRequest()),
 
-                new MenuOption("Log Out", () -> {
-                    this.logOut();; validChoice[0] = 1;})
+            new MenuOption("Log Out", () -> 
+                this.logOut())
 
-            ).readOption(context);
-
-            if(validChoice[0] != 1){
-                new Text("Invalid choice. Please select a valid option.", TextStyle.BOLD).paint(context);
-                View.gotoPrevNthLine(2);
-                View.clearLines(1);
-            }
-        }
+        ).readOption(context);
     }
     
 }
