0cc4884de9b27d94beb8dc82a4e4ed9e25bb897b
Bryan Soong <whiterevolver35@gmail.com>
2024-11-10 15:24:00 +0800

    :fire: feat: basic inventory repository

---
+++ src/repository/InventoryRepository.java
@@ -0,0 +1,29 @@
+package repository;
+
+import model.InventoryItem;
+
+public class InventoryRepository extends BaseRepository<InventoryItem> {
+    final static String FILENAME = "users.dat";
+
+    public InventoryRepository() {
+        super(FILENAME);
+    }
+
+    public InventoryItem findByItemName(String itemName) {
+        return findBy(inventoryItem -> inventoryItem.getItemName().equals(itemName)).get(0);
+    }
+
+    public boolean itemExists(String itemName) {
+        return findByItemName(itemName) != null;
+    }
+
+    public int getStockByItemName(String itemName) {
+        return findByItemName(itemName).getStock();
+    }
+
+    public void updateStockByItemName(String itemName, int stock) {
+        InventoryItem inventoryItem = findByItemName(itemName);
+        inventoryItem.setStock(stock);
+        save(inventoryItem);
+    }
+}
