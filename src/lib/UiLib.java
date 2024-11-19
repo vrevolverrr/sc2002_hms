550bff435c13a01cbd7575f204a340567c706b9f
Bryan Soong <whiterevolver35@gmail.com>
2024-11-19 12:16:58 +0800

    added UiLib metadata

---
+++ src/lib/UiLib.java
@@ -0,0 +1,24 @@
+package lib;
+
+/**
+ * UILib is a declarative UI library that we wrote from scratch, heavily inspired by declarative UI 
+ * libraries like Flutter and React, allowing us to build command line user interfaces using a 
+ * simple and intuitive syntax. 
+ * 
+ * This class serves to provide metadata on the UILib library.
+ * 
+ * @author Bryan Soong
+ * @version 1.0
+ * @since 2024-11-10
+ */
+public class UiLib {
+    /**
+     * The version of the UILib.
+     */
+    public static final String VERSION = "1.0";
+
+    /**
+     * The private constructor of the {@link UiLib} class. This class should not be instantiated.
+     */
+    private UiLib() {}
+}
